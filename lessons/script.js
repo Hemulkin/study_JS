64db2f1a94b7e2afc2a2602a60967aa49117ff95
Hemul <evgenii.epanov@gmail.comgit config --global user.email evgenii.epanov@gmail.comgit config --global user.email evgenii.epanov@gmail.com>
2020-04-02 01:14:59 +0500

    урок 3

--- lessons/script.js
+++ lessons/script.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 let money = 30000, income = 40000, 
 addExpenses = 'Интернет, Такси, Коммуналка', 
-deposit = true, mission = 1000000, period = 12;
+deposit = true, mission = 1000000, period = 12, expenses1, expenses2, amount1, amount2;
 
 console.log(typeof(money));
 console.log(typeof(income));
@@ -13,3 +13,6 @@ console.log('Цель заработать ', mission, ' рублей');
 addExpenses.toLowerCase();
 console.log( addExpenses.split(', '));
 
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:');
+deposit = confirm('Есть ли у вас депозит в банке?');
