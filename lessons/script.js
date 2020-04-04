172de519b3ee53793a492941fdc97123de1aba94
Hemul <evgenii.epanov@gmail.comgit config --global user.email evgenii.epanov@gmail.comgit config --global user.email evgenii.epanov@gmail.com>
2020-04-04 20:41:38 +0500

    lesson three complicated

--- lessons/script.js
+++ lessons/script.js
@@ -1,4 +1,37 @@
 /*jshint esversion: 6 */
-let money, income, addExpenses, deposit, mission, period;
-alert('Text!');
-console.log('Text: ', Text);
+//1
+let lang, out;
+const   ru = ['Пн','Вт','Ср','Чт','Пт','Сб','Вс'],
+        en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const obj = {
+    RU : 'Пн Вт Ср Чт Пт Сб Вс ',
+    EN : 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday'
+};
+        lang = prompt('Выберите язык', 'RU');
+        lang.toUpperCase();
+
+if( lang == 'RU'){
+    console.log(ru);
+    document.getElementById('out1').innerHTML = ru;
+} else if( lang == 'EN' ){
+    console.log(en);
+    document.getElementById('out1').innerHTML = en;
+}
+
+switch(lang){
+    case 'RU':
+        document.getElementById('out2').innerHTML = ru;
+    break;
+    case 'EN':
+        document.getElementById('out2').innerHTML = en;
+    break;
+}
+
+document.getElementById('out3').innerHTML = obj[lang];
+
+
+//2
+let namePerson;
+namePerson = prompt();
+out = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
+document.getElementById('out4').innerHTML = out;
