81c4eff7d1a1ce6a9688c0e9f9b9e6e92c010770
Hemul <evgenii.epanov@gmail.comgit config --global user.email evgenii.epanov@gmail.comgit config --global user.email evgenii.epanov@gmail.com>
2020-04-07 21:25:30 +0500

    lesson 6

--- lessons/script.js
+++ lessons/script.js
@@ -1,4 +1,28 @@
-/*jshint esversion: 6 */
-let money, income, addExpenses, deposit, mission, period;
-alert('Text!');
-console.log('Text: ', Text);
+'use strict';
+
+let askQuestion = function(){
+    let hiddenNumber = Math.floor( Math.random() * 100 + 1 );
+    console.log('hiddenNumber: ', hiddenNumber);
+
+    let coreQuestion = function(){
+        let question = prompt('Угадай число от 1 до 100');
+        let questionNumber = Number(question);
+        if ( question === null ){
+            return;
+        } else if (isNaN(question) || question === '' || question === ' ' ){
+            alert('Введите число');
+            coreQuestion();
+        } else if (questionNumber < hiddenNumber){
+            alert('Введенное число меньше загаданного');
+            coreQuestion();
+        } else if (questionNumber > hiddenNumber){
+            alert('Введенное число больше загаданного');
+            coreQuestion();
+        } else if (questionNumber === hiddenNumber ){
+            alert('Верно!');
+        } 
+    };
+    coreQuestion();
+};
+askQuestion();
+
